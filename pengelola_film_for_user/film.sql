77522299a41434fac9201dcaa2948247f6e3257d
yuszspro <fairussuhair@gmail.com>
2025-07-02 14:35:34 +0700

    Delete pengelola_film_for_user directory

--- pengelola_film_for_user/film.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `films` (
-    `id` INT(2) AUTO_INCREMENT PRIMARY KEY,
-    `judul` VARCHAR(255) NOT NULL,
-    `genre` VARCHAR(255) NOT NULL,
-    `tahun_rilis` INT(4) NOT NULL,
-    `status` ENUM('Sudah', 'Belum') NOT NULL
-);
